2fcac0a3ce68263bc39fd254804f99bbc1b911b9
nadia0ostapyshyn <nadia0ostapyshyn@gmail.com>
2024-01-30 19:54:25 +0200

    add hero section

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-import "./fullPageScroll.js";
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.querySelector(".header__burger");
+  const menu = document.querySelector(".header__menu-list");
+
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("open");
+    menu.classList.toggle("visible");
+  });
+});
