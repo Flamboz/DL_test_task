cafab69266a590bad656136e36441a0f1df9d109
nadia0ostapyshyn <nadia0ostapyshyn@gmail.com>
2024-01-31 19:01:51 +0200

    add full page scroll

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,19 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const mySwiper = new Swiper(".swiper-container", {
+  const fullPageSwiper = new Swiper("#page-slider", {
+    direction: "vertical",
+    slidesPerView: 1,
+    mousewheel: true,
+  });
+
+  const presentSwiper = new Swiper("#presents__slider", {
     clickable: true,
     spaceBetween: 30,
     loop: true,
-  });
-
-  mySwiper.on("slideChange", function (swiper) {
-    const activeIndex = swiper.activeIndex;
-    buttons.forEach(function (btn) {
-      btn.classList.remove("active");
-    });
-
-    if (buttons[activeIndex]) {
-      buttons[activeIndex].classList.add("active");
-    }
   });
 
   const buttons = document.querySelectorAll(".present__single-option");
@@ -27,7 +22,18 @@ document.addEventListener("DOMContentLoaded", function () {
       button.classList.add("active");
 
       const swiperId = button.getAttribute("data-swiper-id");
-      mySwiper.slideTo(swiperId);
+      presentSwiper.slideTo(swiperId);
     });
+  });
+
+  presentSwiper.on("slideChange", function (swiper) {
+    const activeIndex = swiper.realIndex;
+    buttons.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+
+    if (buttons[activeIndex]) {
+      buttons[activeIndex].classList.add("active");
+    }
   });
 });
