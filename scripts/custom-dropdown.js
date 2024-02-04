6cf22c8dccdeb0f5abe4de7fdecbbf3abae27110
nadia0ostapyshyn <nadia0ostapyshyn@gmail.com>
2024-02-04 20:34:26 +0200

    update dropdown to support multiple dropdowns

--- scripts/custom-dropdown.js
+++ scripts/custom-dropdown.js
@@ -1,41 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const dropdown = document.querySelector(".dropdown");
-  const dropdownList = dropdown.querySelector(".dropdown__list");
+  const dropdowns = document.querySelectorAll(".dropdown");
 
-  const toggleDropdown = () => {
-    const dropdownBtn = dropdown.querySelector(".dropdown__button");
-    dropdownList.classList.toggle("dropdown__list--visible");
-    dropdownBtn.classList.toggle("dropdown__button--active");
-  };
+  dropdowns.forEach((dropdown) => {
+    const dropdownList = dropdown.querySelector(".dropdown__list");
 
-  const handleListItemClick = (e) => {
-    const dropdownBtn = dropdown.querySelector(".dropdown__button");
-    const dropdownItems = dropdown.querySelectorAll(".dropdown__list-item");
+    const toggleDropdown = () => {
+      const dropdownBtn = dropdown.querySelector(".dropdown__button");
+      dropdownList.classList.toggle("dropdown__list--visible");
+      dropdownBtn.classList.toggle("dropdown__button--active");
+    };
 
-    dropdownItems.forEach(function (el) {
-      el.classList.remove("dropdown__list-item--active");
+    const handleListItemClick = (e) => {
+      const dropdownBtn = dropdown.querySelector(".dropdown__button");
+      const dropdownItems = dropdown.querySelectorAll(".dropdown__list-item");
+
+      dropdownItems.forEach(function (el) {
+        el.classList.remove("dropdown__list-item--active");
+      });
+
+      e.target.classList.add("dropdown__list-item--active");
+      dropdownBtn.innerText = e.target.innerText;
+    };
+
+    dropdown.addEventListener("click", function (e) {
+      if (e.target === dropdown.querySelector(".dropdown__button")) {
+        toggleDropdown();
+      } else if (e.target.classList.contains("dropdown__list-item")) {
+        handleListItemClick(e);
+        toggleDropdown();
+      } else {
+        toggleDropdown();
+      }
     });
 
-    e.target.classList.add("dropdown__list-item--active");
-    dropdownBtn.innerText = e.target.innerText;
-  };
+    document.addEventListener("click", () => {
+      dropdownList.classList.remove("dropdown__list--visible");
+    });
 
-  dropdown.addEventListener("click", function (e) {
-    if (e.target === dropdown.querySelector(".dropdown__button")) {
-      toggleDropdown();
-    } else if (e.target.classList.contains("dropdown__list-item")) {
-      handleListItemClick(e);
-      toggleDropdown();
-    } else {
-      toggleDropdown();
-    }
-  });
-
-  document.addEventListener("click", () => {
-    dropdownList.classList.remove("dropdown__list--visible");
-  });
-
-  dropdown.addEventListener("click", (e) => {
-    e.stopPropagation();
+    dropdown.addEventListener("click", (e) => {
+      e.stopPropagation();
+    });
   });
 });
